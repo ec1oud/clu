c3daf9877f2722d5f7a17f3621b70dda1552c661
Shawn Rutledge <s@ecloud.org>
2025-12-13 23:59:05 +0100

    Add SPDX GPL 3.0 headers, remove license verbosity
    xlog was GPL 3 already; hamlib was GPL 2.1-or-later, so this is
    all compatible. The hamlib code gets bumped up to GPL 3 as well.

--- src/awards_enum.c
+++ src/awards_enum.c
@@ -1,22 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
 /*
    clu - Callsign Looker Upper
    code extracted from xlog - GTK+ logging program for amateur radio operators
    Copyright (C) 2001 - 2010 Joop Stakenborg <pg4i@amsat.org>
    Copyright (C) 2021        Andy Stewart <kb1oiq@arrl.net>
-   Copyright (C) 2025        Shawn Rutledge <s@ecloud.org>
-
-   clu is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
-
-   clu is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with clu.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include <ctype.h>
